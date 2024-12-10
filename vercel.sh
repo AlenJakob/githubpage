18ef2036de57a13eec34b8d4869f194883030b57
Alen <grafuxa@gmail.com>
2024-12-10 20:40:39 +0100

    sh update

--- vercel.sh
+++ vercel.sh
@@ -1,5 +1,16 @@
+#!/bin/bash
+
+# Logowanie gałęzi
+echo "Branch being deployed: $VERCEL_GIT_COMMIT_REF"
+
+# Sprawdzenie, czy gałąź jest "master", jeśli nie to przerywamy proces
 if [[ $VERCEL_GIT_COMMIT_REF != "master" ]]; then
-  echo "Branch being deployed: $VERCEL_GIT_COMMIT_REF"
   echo "Skipping build for non-master branch"
-  exit 0
+  exit 0  # Zatrzymanie skryptu dla innych gałęzi
 fi
+
+# Jeżeli gałąź to "master", kontynuuj build
+echo "Building branch: master"
+
+# Tutaj reszta kroków budowy/deploy
+# np. kompilacja, uruchomienie testów, itd.
